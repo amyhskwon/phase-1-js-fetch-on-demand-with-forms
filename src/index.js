0f138e1f96fc77fe82f0fb54b8221513088f5ef4
amyhskwon <amyhskwon@gmail.com>
2022-11-10 01:20:48 -0800

    commit tree 1b17b92ee9cb39384be6eeabb2d0331f91af89d6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('input#searchByID')
+    console.log(input.value);
+
+    fetch('http://localhost:3000/movies')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
